537c4ef8a4a7af80c8d92167ded4d532a508521b
JD Wilson <jaydee.wilson@gmail.com>
2021-05-14 12:54:28 +0200

    commit

--- src/Form/Form.jsx
+++ src/Form/Form.jsx
@@ -6,33 +6,34 @@ import CoinContext from "../_contexts/CoinContext";
 
 const Form = () => {
   const [amount, setAmount] = useState(0);
+  const [transactionType, setTransactionType ] = useState("buy")
 
-  const { currentCoin , showForm, setShowForm } = useContext(CoinContext);
-  const { id } = currentCoin;
+  const { currentCoin , showForm, setShowForm, wallet, setWallet } = useContext(CoinContext);
+  const { name, market_data } = currentCoin;
   
-  console.log( 'form:', showForm )
+  console.log( currentCoin )
   return (
     showForm &&
     <div className="Form ">
       <div className="Form-Header">
-        <h1> Buy {currentCoin.coinName} </h1>
+        <h1> Buy {name} </h1>
         <div className="Form-Close" onClick={() => setShowForm(false)}> X </div>
       </div>
       <div className="Form-content">
-        <p> Current Price: {currentCoin.currentPrice}</p>
+        <p> Current Price: {market_data.current_price.usd}</p>
         <div className="Input">
           <input type="text" value={amount} />
           <p> Max: {currentCoin.wallet} </p>
         </div>
         <div className="Input-Radio">
-          <div className="buy">
-            <input type="radio" value="buy" name="transaction" /> Buy
+          <div className="transaction">
+            <input type="radio" value="buy" name="transaction" onClick/> Buy
           </div>
           <div className="sell">
             <input type="radio" value="sell" name="transaction" /> Sell
           </div>
         </div>
-        <div className="Form-Button">Buy</div>
+        <div className="Form-Button" onClick={buyCoins}>Buy</div>
       </div>
     </div>
   );
